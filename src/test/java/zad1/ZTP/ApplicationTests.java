fcb07e72ed6aafe9cc70fa42df09fcee495c4033
KrzyzV1 <damian20022@onet.pl>
2025-11-04 18:21:26 +0100

    initial commit + database configuration in application.properties

---
+++ src/test/java/zad1/ZTP/ApplicationTests.java
@@ -0,0 +1,13 @@
+package zad1.ZTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
